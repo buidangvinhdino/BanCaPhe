ae8a6c357781db7eba7f8914e06a6701db527ebc
Vinh <buidangvinhdino@gmail.com>
2023-11-27 19:15:22 +0700

    Acc frg

--- app/src/main/java/com/example/bancaphe/MainActivity.java
+++ app/src/main/java/com/example/bancaphe/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.example.bancaphe.FragmentManager.Account_Fragment;
 import com.example.bancaphe.FragmentManager.HomeFrgm;
 import com.example.bancaphe.FragmentManager.ProductFrgm;
 import com.example.bancaphe.FragmentManager.StoreFrgm;
